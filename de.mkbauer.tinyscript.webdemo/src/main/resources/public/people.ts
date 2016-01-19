4fae608033b10e7238ad222c4a53d4ec737e75c1
Markus Bauer <Markus.Bauer@cas.de>
2016-01-19 13:40:18 +0100

    Add mini benchmark to evaluate the efficiency of object tracking.

---
+++ de.mkbauer.tinyscript.webdemo/src/main/resources/public/people.ts
@@ -0,0 +1,65 @@
+// Creates an array of "people" and to some searching on it.
+
+// Size of the array
+var N = 1000;
+
+// Repetions
+var R = 10;
+
+function randomInt(min, max) {
+    return Math.round(Math.random() * (max - min -1)) + min;
+}
+
+var CHRISTIAN_NAMES = ['Anna', 'Emilia', 'Emma', 'Hannah', 'Lea', 'Lena', 'Leonie', 
+	'Marie', 'Mia', 'Sophia', 'Ben', 'Elias', 'Fynn', 'Jonas', 'Leon', 'Louis', 
+	'Luca', 'Lukas', 'Noah', 'Paul'];
+var NAMES = ['Mueller', 'Schmidt', 'Schneider', 'Fischer', 'Meyer', 'Weber', 'Hofmann', 
+	'Wagner', 'Becker', 'Schulz', 'Schaefer', 'Koch', 'Bauer', 'Richter', 'Klein', 
+	'Schroeder', 'Wolf', 'Neumann', 'Schwarz'];
+var CITIES = ['Hambug', 'Karlsruhe', 'Muenchen', 'Stuttgart', 'New-York', 
+	'Konstanz', 'Paris', 'Madrid', 'Rom', 'Mailand', 'Genua', 'Stockholm', 
+	'Kopenhagen', 'Washington', 'Moskau', 'Schenkenzell'];
+
+function Person() {}
+
+Person.prototype.toString = function () {
+    return this.christianName + " " + this.name + ", " + this.street + ", " + this.city;
+};
+    
+function createPerson() {
+	var result = new Person();
+	result.name = NAMES[randomInt(0, NAMES.length)] + "-" + NAMES[randomInt(0, NAMES.length)];
+	result.christianName = CHRISTIAN_NAMES[randomInt(0, CHRISTIAN_NAMES.length)] 
+		+ "-" + CHRISTIAN_NAMES[randomInt(0, CHRISTIAN_NAMES.length)];
+	result.street = CHRISTIAN_NAMES[randomInt(0, CHRISTIAN_NAMES.length)] 
+		+ "-" + NAMES[randomInt(0, NAMES.length)] + "-Str. " + randomInt(1, 1000);
+	result.city = randomInt(10000, 90000) + " " + CITIES[randomInt(0, CITIES.length)];
+	return result;
+}
+
+function createPeople(n) {
+	var people = [];
+	for (var i = 0, n-1) {
+		people.push(createPerson());
+	}
+    return people;
+}
+
+function countBauers(people) {
+    var result = 0;
+	for (var i = 0, people.length - 1) {
+        if (people[i].name.indexOf("Bauer") >= 0) {
+            result = result +1;
+        }
+    }
+    return result;
+}
+
+function test() {         
+	for (var i = 0, R-1) {
+		var people = createPeople(N);
+		print(countBauers(people));
+	}
+}
+
+test();
---
+++ de.mkbauer.tinyscript.webdemo/src/test/java/de/mkbauer/tinyscript/webdemo/MultipleSessionsTest.java
@@ -0,0 +1,64 @@
+package de.mkbauer.tinyscript.webdemo;
+
+import java.io.InputStream;
+import java.util.Map;
+
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import de.mkbauer.tinyscript.interpreter.ResourceConsumption;
+
+public class MultipleSessionsTest {
+	
+	private static final int THREADS = 10;
+	private static final String PUBLIC_FOLDER = "public";
+	private static final String URI = "http://localhost:8080/execute";
+	
+	private static class Session implements Runnable {
+		
+		private String script;
+		
+		public Session(String script) {
+			this.script = script;
+		}
+
+		@Override
+		public void run() {
+			runScriptOnServer(script);	
+		}
+		
+	}
+	    
+	private static String convertStreamToString(java.io.InputStream in) {
+	    java.util.Scanner s = new java.util.Scanner(in).useDelimiter("\\A");
+	    String result = s.hasNext() ? s.next() : "";
+	    return result;
+	}	
+	
+	public static String readScriptFromFile(String filename) {
+		InputStream in = MultipleSessionsTest.class.getClassLoader()
+                .getResourceAsStream(PUBLIC_FOLDER + java.io.File.separator + filename);
+		String script = convertStreamToString(in);
+		return script;
+	}
+	
+	public static void runScriptOnServer(String script) {
+		System.out.println("Running script...");
+	    RestTemplate restTemplate = new RestTemplate();
+	    TinyscriptExecutionResult result = restTemplate.postForObject(URI, script, TinyscriptExecutionResult.class); 
+	    ResourceConsumption statistics = result.getStatistics();
+	    System.out.println("Results: time=" + statistics.getExecutionTime() +"ms"
+	    		+ ", objs=" + statistics.getObjects() + "/" + statistics.getObjectsMax()
+	    		+ ", mem=" + statistics.getMemory()/1024 + "K/" + statistics.getMemoryMax()/1014 + "K"
+	    		+ ", creates=" + statistics.getObjectCreations());
+	}
+	
+	public static void main(String[] args) {
+		for (int i = 1; i <= THREADS; i++) {
+			Thread test = new Thread(new Session(readScriptFromFile("people.ts")));
+	        test.start();
+		}
+	}
+
+}
